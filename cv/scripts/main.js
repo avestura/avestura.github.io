151ba1a417d949dd3a1c99b9c338ad663e6b7a0f
Aryan <aryanebrahimpour@gmail.com>
2018-09-27 01:35:55 +0330

    Disable AOS

--- cv/scripts/main.js
+++ cv/scripts/main.js
@@ -2,10 +2,7 @@
 // Don't forget to add it into respective layouts where this js file is needed
 
 $(document).ready(function() {
-  AOS.init( {
-    // uncomment below for on-scroll animations to played only once
-    // once: true  
-  }); // initialize animate on scroll library
+
 });
 
 // Smooth scroll for links with hashes

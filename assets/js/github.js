381f884fa77f5a560667a3aeb5d1459e3d26512a
Aryan <oxaryan@outlook.com>
2019-11-03 20:53:32 +0330

    refuctor

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,31 +1,31 @@
 (async () => {
-const $restConf = {
+const restConfig = {
     method: "GET",
     headers: {
         'Content-Type': 'application/json',
         "Authorization": "token a2626cd7f6bfeaf960f11fb390d3f1ddd279c377"
     }
 };
-const userReq = await fetch('https://api.github.com/users/0xaryan', $restConf);
-const repoReq = await fetch('https://api.github.com/users/0xaryan/repos?per_page=100', $restConf);
+const userReq = await fetch('https://api.github.com/users/0xaryan', restConfig);
+const repoReq = await fetch('https://api.github.com/users/0xaryan/repos?per_page=100', restConfig);
 const userResp = await userReq.json();
 const repoResp = await repoReq.json();
 
-const $ghProfileImage     = document.getElementById("gh-profile-image"),
-      $ghProfileName      = document.getElementById("gh-profile-name"),
-      $ghProfileBio       = document.getElementById("gh-profile-bio"),
-      $ghProfileRepos     = document.getElementById("gh-profile-repos"),
-      $ghProfileGists     = document.getElementById("gh-profile-gists"),
-      $ghProfileFollowers = document.getElementById("gh-profile-followers"),
-	  $ghRepoContainer    = document.getElementById("gh-repo-container")
-	  $loadingRow         = document.getElementById("gh-loading-row");
+const ghProfileImage     = document.getElementById("gh-profile-image"),
+      ghProfileName      = document.getElementById("gh-profile-name"),
+      ghProfileBio       = document.getElementById("gh-profile-bio"),
+      ghProfileRepos     = document.getElementById("gh-profile-repos"),
+      ghProfileGists     = document.getElementById("gh-profile-gists"),
+      ghProfileFollowers = document.getElementById("gh-profile-followers"),
+	  ghRepoContainer    = document.getElementById("gh-repo-container"),
+	  loadingRow         = document.getElementById("gh-loading-row");
 
-$ghProfileImage.src = userResp.avatar_url;
-$ghProfileName.innerText = userResp.name;
-$ghProfileBio.innerText = userResp.bio;
-$ghProfileFollowers.innerText = userResp.followers;
-$ghProfileRepos.innerText = userResp.public_repos;
-$ghProfileGists.innerText = userResp.public_gists;
+ghProfileImage.src = userResp.avatar_url;
+ghProfileName.innerText = userResp.name;
+ghProfileBio.innerText = userResp.bio;
+ghProfileFollowers.innerText = userResp.followers;
+ghProfileRepos.innerText = userResp.public_repos;
+ghProfileGists.innerText = userResp.public_gists;
 
 const createAuthorNode = isMe => {
     const $authorNode = document.createElement("span");
@@ -34,31 +34,40 @@ const createAuthorNode = isMe => {
     else    { $authorNode.innerHTML = '<span class="tag-avatar avatar""><i class="fa fa-code-fork"></i></span>Fork'; }
     return $authorNode;
 }
-const formatSize = size => {
-    if(size < 1500) { return `${size} KB`}
-    else {return `${(size/1000.0).toFixed(1)} MB` }
-}
+
+const formatSize = size => (size < 1500) ? `${size} KB` : `${(size/1000.0).toFixed(1)} MB`;
 
 const createLangNode = lang => {
     if(lang != null) {
         const $langTag = document.createElement("div");
-        $langTag.classList.add("tag");
-        $langTag.classList.add("tag-info");
-        $langTag.innerHTML =
-        `${lang}<span class="tag-addon"><i class="fe fe-code"></i></span>`;
-
+        $langTag.classList.add("tag", "tag-info");
+        $langTag.innerHTML = `${lang}<span class="tag-addon"><i class="fe fe-code"></i></span>`;
         return $langTag;
     }
     else return document.createElement("div");
 };
+
 const applyFormat = date => `${date.substr(0, 4)}`;
-const createRow = (id, repoName, repoDesc, repoUrl, createdDate, size, stars, forks, issues, ownerUrl, license) =>{
 
-    const $rowNode = document.createElement("tr");
+const createRow = response => {
+	const r = response;
+	const id = r.id,
+	      repoName = r.name,
+	      repoDesc = r.description || "(No description provided)",
+	      repoUrl = r.html_url,
+	      createdDate = r.created_at,
+	      size = r.size,
+	      stars = r.stargazers_count,
+	      forks = r.forks,
+	      issues = r.open_issues_count,
+	      ownerUrl = r.owner.html_url,
+	      license = r.license;
+
+    const rowNode = document.createElement("tr");
     const rawLicense = (license || {name: "No License"} ).name;
-    const lIconFe = license == null ? "x" : "check";
+    const iconTag = !license ? "x" : "check";
 
-    $rowNode.innerHTML = `
+    rowNode.innerHTML = `
     <td>
         <div><span style="cursor: help" data-toggle="tooltip" title="${repoDesc}" data-original-title="${repoDesc}">${repoName}</span></div>
         <div class="small text-muted">
@@ -85,32 +94,22 @@ const createRow = (id, repoName, repoDesc, repoUrl, createdDate, size, stars, fo
                         class="dropdown-icon fe fe-user"></i> Visit Owner </a>
                 <div class="dropdown-divider"></div>
                 <a href="javascript:void(0)" class="dropdown-item disabled"><i
-                        class="dropdown-icon fe fe-${lIconFe}-circle"></i> ${rawLicense}</a>
+                        class="dropdown-icon fe fe-${iconTag}-circle"></i> ${rawLicense}</a>
             </div>
         </div>
     </td>
     `;
 
-    return $rowNode;
+    return rowNode;
 }
 
-$ghRepoContainer.removeChild($loadingRow)
+ghRepoContainer.removeChild(loadingRow)
 repoResp.sort((x, y) => y.stargazers_count - x.stargazers_count);
 repoResp.forEach(r => {
 
-    const $rowNode = createRow(r.id,
-                               r.name,
-                               r.description || "(No description provided)",
-                               r.html_url,
-                               r.created_at,
-                               r.size,
-                               r.stargazers_count,
-                               r.forks,
-                               r.open_issues_count,
-                               r.owner.html_url,
-                               r.license);
+    const rowNode = createRow(r);
 
-    $ghRepoContainer.appendChild($rowNode);
+    ghRepoContainer.appendChild(rowNode);
     document.getElementById(`author-node-${r.id}`).appendChild(createAuthorNode(!r.fork));
     document.getElementById(`lang-${r.id}`).appendChild(createLangNode(r.language));
     if(r.archived || r.disabled){document.getElementById(`status-${r.id}`).innerHTML = '<div class="tag tag-purple">archived<span class="tag-addon"><i class="fe fe-book"></i></span></div>';
